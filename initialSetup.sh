06f325bc97019dbdfb518aed1e467a28033c8ee4
NuxDD <maxime.desousa31@gmail.com>
2020-12-05 21:47:54 +0100

    fix native linux distrib issue

--- initialSetup.sh
+++ initialSetup.sh
@@ -5,17 +5,18 @@ USER=`whoami`
 # initial setup for WSL2 on W10
 if grep -q Microsoft /proc/version; then
 	$WUSER=$(/mnt/c/Windows/System32/cmd.exe /c 'echo %USERNAME%')
-	ln -s "/mnt/c/Users/$WUSER" ~/winhome
+	ln -s "/mnt/c/Users/${WUSER}" ~/winhome
 	if [ ! -d "/mnt/c/Users/$WUSER/repos" ]; then
 		mkdir "/mnt/c/Users/$WUSER/repos"
 	fi
-	ln -s "/mnt/c/Users/$WUSER/repos" ~/repos
+	ln -s "/mnt/c/Users/${WUSER}repos" ~/repos
+else
+	# just a sanity check to create the repos folder on a native Linux system
+	if [ ! -d ~/repos ]; then
+		mkdir ~/repos
+	fi
 fi
 
-# just a sanity check to create the repos folder on a native Linux system
-if [ ! -d ~/repos ]; then
-	mkdir ~/repos
-fi
 
 # touching the file to avoid an unnecessary push while using checkUpdate.sh
 cp ./files/.vimrc ~/ & touch -m ./files/.vimrc
